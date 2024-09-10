8020d5c0e720b16b20639ab90bd27c1e227a7994
william wollert <williamwollert@gmail.com>
2024-09-10 17:38:42 -0300

    v1.1

--- Dijkstra.py
+++ Dijkstra.py
@@ -1,40 +1,52 @@
 import heapq
 
-def dijkstra(graph, start):
-    # Dicionário para armazenar a menor distância de cada nó
+def dijkstra(graph, start, end):
     distances = {node: float('infinity') for node in graph}
     distances[start] = 0
 
-    # Fila de prioridade para processar os nós
-    priority_queue = [(0, start)]  # (distância, nó)
+    previous_nodes = {node: None for node in graph}
+
+    priority_queue = [(0, start)]
 
     while priority_queue:
         current_distance, current_node = heapq.heappop(priority_queue)
 
-        # Se a distância do nó já for maior, não precisamos processá-lo
+        if current_node == end:
+            break
+
         if current_distance > distances[current_node]:
             continue
 
-        # Verificando os vizinhos do nó atual
         for neighbor, weight in graph[current_node].items():
             distance = current_distance + weight
 
-            # Se encontrarmos um caminho mais curto para o vizinho
             if distance < distances[neighbor]:
                 distances[neighbor] = distance
+                previous_nodes[neighbor] = current_node
                 heapq.heappush(priority_queue, (distance, neighbor))
 
-    return distances
+    path = []
+    current_node = end
+    while current_node is not None:
+        path.insert(0, current_node)
+        current_node = previous_nodes[current_node]
 
-# Exemplo de uso:
+    if distances[end] == float('infinity'):
+        return (float('infinity'), [])
+
+    return (distances[end], path)
+
 graph = {
-    'Rio do Sul': {'B': 1, 'C': 4},
+    'A': {'B': 1, 'C': 4},
     'B': {'A': 1, 'C': 2, 'D': 5},
     'C': {'A': 4, 'B': 2, 'D': 1},
-    'D': {'B': 5, 'C': 1}
+    'D': {'B': 5, 'C': 1,'E':3},
+    'E': {'D':3}
 }
 
-start_node = 'A'
-shortest_paths = dijkstra(graph, start_node)
+start_node = input(F"Informe qual a cidade de partida: ")
+end_node = input(F"Informe qual a cidade de destino: ")
+distance, path = dijkstra(graph, start_node, end_node)
 
-print(f"Distâncias a partir do nó {start_node}: {shortest_paths}")
+print(f"Distância mais curta de {start_node} até {end_node}: {distance}")
+print(f"Caminho: {path}")
