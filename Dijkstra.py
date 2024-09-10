622dfba3e057d7974e2bcaa6216f43c33ff28b39
William Wollert <williamwollert@gmail.com>
2024-09-10 20:17:52 -0300

    v1.2

--- Dijkstra.py
+++ Dijkstra.py
@@ -1,52 +1,64 @@
 import heapq
 
-def dijkstra(graph, start, end):
-    distances = {node: float('infinity') for node in graph}
-    distances[start] = 0
+def dijkstra(grafo, ini, fim):
+    distancias = {no: float('infinity') for no in grafo}
+    distancias[ini] = 0
 
-    previous_nodes = {node: None for node in graph}
+    no_anterior = {no: None for no in grafo}
 
-    priority_queue = [(0, start)]
+    prioridade = [(0, ini)]
 
-    while priority_queue:
-        current_distance, current_node = heapq.heappop(priority_queue)
+    while prioridade:
+        distancia_atual, no_atual = heapq.heappop(prioridade)
 
-        if current_node == end:
+        if no_atual == fim:
             break
 
-        if current_distance > distances[current_node]:
+        if distancia_atual > distancias[no_atual]:
             continue
 
-        for neighbor, weight in graph[current_node].items():
-            distance = current_distance + weight
+        for vizinho, peso in grafo[no_atual].items():
+            distancia = distancia_atual + peso
 
-            if distance < distances[neighbor]:
-                distances[neighbor] = distance
-                previous_nodes[neighbor] = current_node
-                heapq.heappush(priority_queue, (distance, neighbor))
+            if distancia < distancias[vizinho]:
+                distancias[vizinho] = distancia
+                no_anterior[vizinho] = no_atual
+                heapq.heappush(prioridade, (distancia, vizinho))
 
-    path = []
-    current_node = end
-    while current_node is not None:
-        path.insert(0, current_node)
-        current_node = previous_nodes[current_node]
+    caminho = []
+    no_atual = fim
+    while no_atual is not None:
+        caminho.insert(0, no_atual)
+        no_atual = no_anterior[no_atual]
 
-    if distances[end] == float('infinity'):
+    if distancias[fim] == float('infinity'):
         return (float('infinity'), [])
 
-    return (distances[end], path)
+    return (distancias[fim], caminho)
 
-graph = {
-    'A': {'B': 1, 'C': 4},
-    'B': {'A': 1, 'C': 2, 'D': 5},
-    'C': {'A': 4, 'B': 2, 'D': 1},
-    'D': {'B': 5, 'C': 1,'E':3},
-    'E': {'D':3}
+grafo = {
+    'rio do sul': {'taio':66,'aurora':132,'ituporanga':132,'agronomica':231,'mirim doce':363,'presidente getulio':231},
+    'taio': {'rio do sul':66,'pouso redondo':90,'aurora':30,'ibirama':240,'laurentino':120},
+    'aurora': {'taio':30,'rio do sul':132,'ituporanga':60,'santa teresinha':60},
+    'ituporanga': {'rio do sul':132,'aurora':60,'santa teresinha':45,'salete':60},
+    'pouso redondo': {'taio':90,'presidente getulio':55},
+    'presidente getulio': {'pouso redondo':55,'laguna':66,'mirim doce':198,'rio do sul':231,'ibirama':231},
+    'mirim doce': {'mirim doce':363,'presidente getulio':198,'agronomica':30},
+    'agronomica':{'mirim doce':30,'rio do sul':231},
+    'laguna':{'presidente getulio':66},
+    'ibirama':{'presidente getulio':231,'taio':240},
+    'laurentino':{'taio':120},
+    'santa teresinha':{'aurora':60,'ituporanga':45,'atalanta':88},
+    'atalanta':{'santa teresinha':88,'salete':140,'agrolandia':65},
+    'salete':{'atalanta':140,'agrolandia':60,'ituporanga':60},
+    'agrolandia':{'atalanta':65,'salete':60}
 }
 
-start_node = input(F"Informe qual a cidade de partida: ")
-end_node = input(F"Informe qual a cidade de destino: ")
-distance, path = dijkstra(graph, start_node, end_node)
-
-print(f"Distância mais curta de {start_node} até {end_node}: {distance}")
-print(f"Caminho: {path}")
+op = 'S'
+while op == 'S':
+    partida = input(F"Informe qual a cidade de partida: ")
+    chegada = input(F"Informe qual a cidade de destino: ")
+    distancia, caminho = dijkstra(grafo, partida, chegada)
+    print(f"Distância mais curta de {partida} até {chegada}: {distancia}")
+    print(f"Caminho: {caminho}")
+    op = input('Deseja fazer outra consulta? (S ou N)')
